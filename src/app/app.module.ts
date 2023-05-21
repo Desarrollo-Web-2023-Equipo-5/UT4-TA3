2f0db4df9bcf16782a2051c62fd7f30e61be3df2
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-05-21 19:15:04 -0300

    fixed nav routing and removed unnecesary comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { NavButtonComponent } from './nav-button/nav-button.component';
-import { DiscoverIndexComponent } from './discover-index/discover-index.component';
-import { DiscoverPostComponent } from './discover-post/discover-post.component';
-import { DiscoverFeedComponent } from './discover-feed/discover-feed.component';
-import { UserInfoComponent } from './user-info/user-info.component';
 import { SearchComponent } from './pages/search/search.component';
 import { UploadComponent } from './pages/upload/upload.component';
 import { ChatComponent } from './pages/chat/chat.component';
@@ -19,11 +14,6 @@ import { DiscoverComponent } from './pages/discover/discover.component';
   declarations: [
     AppComponent,
     NavComponent,
-    NavButtonComponent,
-    DiscoverIndexComponent,
-    DiscoverPostComponent,
-    DiscoverFeedComponent,
-    UserInfoComponent,
     SearchComponent,
     UploadComponent,
     ChatComponent,
