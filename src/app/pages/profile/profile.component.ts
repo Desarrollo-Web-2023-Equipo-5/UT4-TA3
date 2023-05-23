35c68bb18e631dd03f48b01fadad68917069515e
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-05-23 18:50:58 -0300

    image service and profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
 import { User } from 'src/app/interfaces/user';
+import { ImagesService } from 'src/app/services/images.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -10,12 +12,16 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfileComponent implements OnInit {
   
   user!: User;
+  posts!: Post[]
 
-  constructor( private userService: UserService ) {}
+  constructor( private userService: UserService, private imageService: ImagesService ) {}
 
   ngOnInit(): void {
     this.userService.getUser().subscribe({
       next: user => this.user = user
+    })
+    this.imageService.getPostsByUsername(this.user.username).subscribe({
+      next: posts => this.posts = posts
     })
   }
 
