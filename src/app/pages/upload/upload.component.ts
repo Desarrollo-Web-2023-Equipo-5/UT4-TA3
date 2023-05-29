ab7ba6bf52ce665ef098f79720b53048c4ba0ccc
JNocetti <juan.anocetti@gmail.com>
2023-05-29 20:20:53 -0300

    upload

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  selectedFile: File | null = null;
 
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    if (this.selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const fileData = e.target.result;
+        console.log('Archivo subido exitosamente:', fileData);
+      };
+      reader.readAsDataURL(this.selectedFile);
+    } else {
+      console.log('No se ha seleccionado ningún archivo.');
+    }
+  }
 }
