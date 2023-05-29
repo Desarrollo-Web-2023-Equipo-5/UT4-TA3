109c0b9a2e56534e394285098227c37bde36747f
PieroSaucedo <piero.saucedo@correo.ucu.edu.uy>
2023-05-29 19:54:00 -0300

    chat overlay and mock changes

---
+++ src/app/mock/users.mock.ts
@@ -0,0 +1,32 @@
+import { User } from "../interfaces/user";
+
+export const USERS: User[] = [
+    {
+        name: 'Jane',
+        surname: 'Doe',
+        username: '@Jane',
+        location: 'San Francisco, CA',
+        profilePhoto: 'https://media.istockphoto.com/id/1327592692/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-woman.jpg?s=612x612&w=0&k=20&c=y-dvtlLq6ksJ9aJXkkw2prwGwSiQvY37JfPpb73wYTc=',
+    },
+    {
+        name: 'John',
+        surname: 'Doe',
+        username: '@John',
+        location: 'San Francisco, CA',
+        profilePhoto: 'https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=',
+    },
+    {
+        name: 'Josiah',
+        surname: 'Doe',
+        username: '@Josiah',
+        location: 'San Francisco, CA',
+        profilePhoto: 'https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_1280.png',
+    },
+    {
+        name: 'Jenny',
+        surname: 'Doe',
+        username: '@Jenny',
+        location: 'San Francisco, CA',
+        profilePhoto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7tZpFfLinld2zgZCL6lAngl1qCnfpqLAolg&usqp=CAU',
+    },
+]
