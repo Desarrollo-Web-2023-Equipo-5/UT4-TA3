8a6e61a0d6d9072de7bd10b670326a91ff6f8e52
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-05-31 15:49:57 -0300

    close overlay when changing route

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-overlay',
@@ -9,4 +10,16 @@ export class OverlayComponent {
 
   @Output() closeOverlay = new EventEmitter(); 
   @Input() currentOverlay!: string;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe({
+      next: (event) => {
+        if (event instanceof NavigationEnd) {
+          this.closeOverlay.emit();
+        }
+      }
+    });
+  }
 }
